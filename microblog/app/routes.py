8c1967f27593126aa3cff36ccaeef7f7fa89cc1b
Konstantin Yakovlev <morggoth70@gmail.com>
2021-05-10 01:20:14 +0300

    Add first simple template

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,15 +1,9 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
+    # Mock the user object
     user = {'username': 'Morggoth'}
-    return f'''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, {user['username']}!</h1>
-    </body>
-</html>'''
+    return render_template('index.html', title='Home', user=user)
