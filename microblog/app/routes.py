d22b3b259b6fd4fa7c695a9523c084006be36d81
Konstantin Yakovlev <morggoth70@gmail.com>
2021-05-10 00:59:59 +0300

    Add simple HTML codo to the view function

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -3,4 +3,13 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello world from the Flask!"
+    user = {'username': 'Morggoth'}
+    return f'''
+<html>
+    <head>
+        <title>Home Page - Microblog</title>
+    </head>
+    <body>
+        <h1>Hello, {user['username']}!</h1>
+    </body>
+</html>'''
