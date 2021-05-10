3a7f160b43d3e6f2d91ebe1384c7028dfe31fc69
Konstantin Yakovlev <morggoth70@gmail.com>
2021-05-10 03:14:17 +0300

    Add processing of the data from the form

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -20,7 +20,13 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    # Allows us to process the form. When the browser sends the GET request, it returns False and skips,
+    # but on the POST request it returns True, gathers all the data in the request and apply all validators
+    if form.validate_on_submit():
+        # flash() shows a message to the user
+        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
